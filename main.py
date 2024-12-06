03e32b737131ca1f55dbe39b97eb45023e6ff11c
zkom01 <komarek.zdenek@seznam.cz>
2024-12-06 22:54:55 +0100

    teměř hotovo

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+class KalkulackaOOP:
+
+    def nacti_cislo(self,text_zadani,text_chyba):
+        while True:
+            try:
+                cislo = int(input(text_zadani).strip())
+                return cislo
+            except ValueError:
+                print(text_chyba)
+
+    def proved_operaci(self, a, b):
+        operace = 0
+        kalc = KalkulackaOOP()
+        while operace not in [1,2,3,4]:
+            print("1 - sčítání")
+            print("2 - odčítání")
+            print("3 - násobení")
+            print("4 - dělení")
+
+            operace = kalc.nacti_cislo("Volba:","Zadej číslo 1 - 4")
+
+            if 1 <= operace <= 4:
+                match operace:
+                    case 1:
+                        return a + b
+                    case 2:
+                        return a - b
+                    case 3:
+                        return a * b
+                    case 4:
+                        return a / b if b != 0 else "Nulou nelze dělit!!"
+            else:
+                print("Zadej číslo 1 - 4")
+
 print("Vítejte v Kalkulačce OOP")
+kalc = KalkulackaOOP()
 
-a = int(input("Zadejte první číslo: "))
-b = int(input("Zadejte druhé čislo: "))
+a = kalc.nacti_cislo("Zadej první číslo:", "Není číslo znovu!")
+b = kalc.nacti_cislo("Zadej druhé číslo:", "Není číslo znovu!")
 
-soucet = a + b
-rozdil = a - b
-soucin = a * b
-podil = a / b
-
-print(f"soucet : {soucet}")
-print(f"rozdil: {rozdil}")
-print(f"soucin: {soucin}")
-print(f"podil: {podil}")
+print(f"Zadali jste čísla {a} a {b} výsledek: {kalc.proved_operaci(a, b)}")
