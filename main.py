30e5b2d6da343b1ba5cdde0a5aaa745f20605c5c
zkom01 <komarek.zdenek@seznam.cz>
2024-12-06 21:12:47 +0100

    zaciname

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Vítejte v Kalkulačce OOP")
+
+cislo1 = int(input("Zadejte první číslo: "))
+cislo2 = int(input("Zadejte druhé čislo: "))
+
+soucet = cislo1 + cislo2
+rozdil = cislo1 - cislo2
+soucin = cislo1 * cislo2
+podil = cislo1 / cislo2
+
+print(f"soucet : {soucet}")
+print(f"rozdil: {rozdil}")
+print(f"soucin: {soucin}")
+print(f"podil: {podil}")
