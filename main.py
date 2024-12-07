ab9e996040d813d78bfaf35b2698607d6f1d91db
zkom01 <komarek.zdenek@seznam.cz>
2024-12-07 01:06:06 +0100

    teměř hotovo

--- main.py
+++ main.py
@@ -21,23 +21,36 @@ class KalkulackaOOP:
             if 1 <= operace <= 4:
                 match operace:
                     case 1:
-                        return a + b
+                        return f"Součet: {a + b}"
                     case 2:
-                        return a - b
+                        return f"Rozdíl: {a - b}"
                     case 3:
-                        return a * b
+                        return f"Součin: {a * b}"
                     case 4:
-                        return a / b if b != 0 else "Nulou nelze dělit!!"
+                        return f"Podíl: {a / b}" if b != 0 else "Chyba: Nulou nelze dělit!"
             else:
                 print("Zadej číslo 1 - 4")
+
+    def znovu_nebo_ukoncit(self):
+        while True:
+            volba = input("Chcete pokračovat? (ano/ne): ").strip().lower()
+            if volba in ["ano", "a"]:
+                return True
+            elif volba in ["ne", "n"]:
+                return False
+            else:
+                print("Zadejte 'ano' nebo 'ne'.")
 
 
 print("Vítejte v Kalkulačce OOP")
 kalc = KalkulackaOOP()
 
-cislo1 = kalc.nacti_cislo("Zadej první číslo:", "Není číslo znovu!")
-cislo2 = kalc.nacti_cislo("Zadej druhé číslo:", "Není číslo znovu!")
+while True:
+    cislo1 = kalc.nacti_cislo("Zadej první číslo: ", "Není číslo, zkuste znovu!")
+    cislo2 = kalc.nacti_cislo("Zadej druhé číslo: ", "Není číslo, zkuste znovu!")
 
-print(f"Zadali jste čísla {cislo1} a {cislo2} výsledek: {kalc.proved_operaci(cislo1, cislo2)}")
+    print(f"Zadali jste čísla {cislo1} a {cislo2}. Výsledek: {kalc.proved_operaci(cislo1, cislo2)}")
 
-# jhdvbdjvbdkjvbdskbvdkvdkvbdkjvbdvkdsn
+    if not kalc.znovu_nebo_ukoncit():
+        print("Děkujeme za použití kalkulačky!")
+        break
